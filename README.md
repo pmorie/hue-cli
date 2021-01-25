59a954713c00390e9735cf47ff21d55ac723ac7c
Paul Morie <pmorie@redhat.com>
2021-01-25 16:45:57 -0500

    add basics to readme

--- README.md
+++ README.md
@@ -7,3 +7,16 @@ A hue CLI client.
 ```
 go build ./cmd/hue-cli
 ```
+
+## setting up a bridge
+
+Use the `bridge discover` command to find a bridge on your network, then `bridge setup` to set that bridge up with the CLI.
+
+```
+$ ./hue-cli bridge discover
+IP             ID
+192.168.50.100 001788fffeeeeeee
+
+$ ./hue-cli bridge setup --bridgeIP 192.168.50.100 --user myuser --wait=true
+# you will have to hit the button on the hue bridge after invoking this
+```
--- pkg/cmd/groups.go
+++ pkg/cmd/groups.go
@@ -11,7 +11,10 @@ import (
 )
 
 func init() {
+	groupsGetCmd.PersistentFlags().IntVar(&getGroupParams.id, "id", 1, "ID of the group to get")
+
 	groupsCmd.AddCommand(groupsListCmd)
+	groupsCmd.AddCommand(groupsGetCmd)
 
 	rootCmd.AddCommand(groupsCmd)
 }
@@ -48,13 +51,47 @@ var groupsListCmd = &cobra.Command{
 		})
 
 		w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', 0)
-		fmt.Fprintln(w, "Name\tAny On?")
-		fmt.Fprintln(w, "----\t-------")
+		fmt.Fprintln(w, "Name\tID\tAny On?")
+		fmt.Fprintln(w, "----\t----\t-------")
 
 		for i := range groups {
 			group := groups[i]
-			fmt.Fprintf(w, "%v\t%v\n", group.Name, group.GroupState.AnyOn)
+			fmt.Fprintf(w, "%v\t%v\t%v\n", group.Name, group.ID, group.GroupState.AnyOn)
 		}
+
+		w.Flush()
+	},
+}
+
+type getGroupParamz struct {
+	id int
+}
+
+var getGroupParams = &getGroupParamz{}
+
+var groupsGetCmd = &cobra.Command{
+	Use:   "get",
+	Short: "get a specific group",
+	Long:  "TODO",
+	Run: func(cmd *cobra.Command, args []string) {
+		bridgeIP, user := getLoginFromConfig()
+		bridge := huego.New(bridgeIP, user)
+
+		group, err := bridge.GetGroup(getGroupParams.id)
+		if err != nil {
+			s := fmt.Sprintf("unable to connect to bridge: %v", err)
+			panic(s)
+		}
+
+		w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', 0)
+
+		fmt.Fprintf(w, "Name:\t%v\n", group.Name)
+		fmt.Fprintf(w, "ID:\t%v\n", group.ID)
+		fmt.Fprintf(w, "Type:\t%v\n", group.Type)
+
+		fmt.Fprintln(w, "State:\t")
+		fmt.Fprintf(w, "  Any On:\t%v\n", group.GroupState.AnyOn)
+		fmt.Fprintf(w, "  All On:\t%v\n", group.GroupState.AllOn)
 
 		w.Flush()
 	},
